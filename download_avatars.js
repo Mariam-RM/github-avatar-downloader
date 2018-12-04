5be84bf8b0179867754c2aeded67f0491e1834ff
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-04 13:42:33 +0000

    finished step7

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,9 @@
 var request = require('request');
-var mytoken = require('./secrets.js')
+var mytoken = require('./secrets.js');
+// var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function request (options, cb) {
-
-}
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
@@ -23,7 +21,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
     cb(err, obj)
   })
 
-   // ...
 }
 
 var iterateOverResults =  function(err, result) {
@@ -33,6 +30,22 @@ var iterateOverResults =  function(err, result) {
   }
 };
 
+function downloadImageByURL(url,path) {
+
+var fs = require('fs');
+
+request.get(url)
+    .on('error', function (err) {
+      throw err;
+    })
+    .on('response', function (response) {
+     console.log('Response Status Code: ', response.statusCodxe);
+     console.log('Response Message: ', response.statusMessage);
+     console.log('Content type: ', response.headers['content-type']);
+    })
+    .pipe(fs.createWriteStream(path));
+}
+
 getRepoContributors("jquery", "jquery", iterateOverResults);
 
 
@@ -40,3 +53,4 @@ getRepoContributors("jquery", "jquery", iterateOverResults);
 //Your next and final step in this exercise should be to change your getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
 
 // You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
