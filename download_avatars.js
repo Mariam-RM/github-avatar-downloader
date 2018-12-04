40fe12c5338a4f3e65e6f7f26c88bf8c100989a0
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-04 00:25:36 +0000

    finished step6

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,19 @@
 var request = require('request');
+var mytoken = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  var options = {
+
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': token + mytoken.GITHUB_TOKEN
+    }
+  };
+
   request(url, function(err, res, body) {
     cb(err, body);
   })
