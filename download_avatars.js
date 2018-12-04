e93cd37adb861f3a375c205e28a3cd0753d99169
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-04 03:49:23 +0000

    finished with step6

--- download_avatars.js
+++ download_avatars.js
@@ -3,6 +3,10 @@ var mytoken = require('./secrets.js')
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function request (options, cb) {
+
+}
+
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
@@ -10,18 +14,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': token + mytoken.GITHUB_TOKEN
+      'Authorization': 'token' + mytoken.GITHUB_TOKEN
     }
   };
 
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    var obj = JSON.parse(body);
+    cb(err, obj)
   })
+
    // ...
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
+var iterateOverResults =  function(err, result) {
 
+  for (key in result){
+    console.log(result[key].avatar_url);
+  }
+};
+
+getRepoContributors("jquery", "jquery", iterateOverResults);
+
+
+
+//Your next and final step in this exercise should be to change your getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
+
+// You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
