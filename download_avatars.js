d4d45cfd0d608a5ddd3e52f1c21a400402316001
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-05 01:11:37 +0000

    finished steps 7and 8

--- download_avatars.js
+++ download_avatars.js
@@ -24,10 +24,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
 }
 
 var iterateOverResults =  function(err, result) {
-
+  var resultsObject = {};
   for (key in result){
-    console.log(result[key].avatar_url);
+    resultsObject.path = result[key].login;
+    resultsObject.avatarUrl = result[key].avatar_url;
+    downloadImageByURL(resultsObject.avatarUrl, resultsObject.path)
   }
+
+  console.log(resultsObject);
+
+
 };
 
 function downloadImageByURL(url,path) {
@@ -39,18 +45,27 @@ request.get(url)
       throw err;
     })
     .on('response', function (response) {
-     console.log('Response Status Code: ', response.statusCodxe);
+     console.log('Response Status Code: ', response.statusCode);
      console.log('Response Message: ', response.statusMessage);
      console.log('Content type: ', response.headers['content-type']);
     })
-    .pipe(fs.createWriteStream(path));
+    .pipe(fs.createWriteStream('avatar/'+ path +'.png'));
 }
 
 getRepoContributors("jquery", "jquery", iterateOverResults);
 
 
 
-//Your next and final step in this exercise should be to change your getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
 
-// You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "kvirani.jpg")
+// downloadImageByURL(avatar_url, "kvirani.jpg")
+
+// With the downloadImageByURL function implemented, you can now connect these two functions by passing in avatar_url from your callback function into downloadImageByURL.
+
+// Here is an English description of how control flows through all your functions:
+
+// getRepoContributors makes a request for JSON, getting back an array of contributors.
+// getRepoContributors passes this data to cb, an anonymous callback function that it is given.
+// cb loops through each item in the array:
+// It constructs a file path using the login value (e.g., "avatars/dhh.jpg")
+// It then passes the avatar_url value and the file path to downloadImageByURL
+// downloadImageByURL fetches the desired avatar_url and saves this information to the given filePath
